013b980b45ed9f23f5511a6b7ea2cd680a527e63
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-22 13:01:05 -0500

    fixed database connection (needed postgresql in URI) and first db migrate with User model

---
+++ app/models.py
@@ -0,0 +1,60 @@
+import secrets
+from . import db
+from datetime import datetime, timezone, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class UserBuyer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    company = db.Column(db.String, nullable=False)
+    token = db.Column(db.String, unique=True)
+    token_exp = db.Column(db.String, unique=True)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    reviews = db.relationship('Review', back_populates='author')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def set_password(self, plaintext_pass):
+        self.password = generate_password_hash(plaintext_pass)
+        self.save()
+
+    def check_password(self, plaintext_pass):
+        return check_password_hash(self.password, plaintext_pass)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            'username': self.username,
+            "title": self.title,
+            'company': self.company
+        }
+    
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_exp.replace(tzinfo=timezone.utc) > now + timedelta(minutes=1):
+            return self.token
+        self.token = secrets.token_hex(16)
+        self.token_exp = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_exp}
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or 'sqlite.///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # or 'sqlite.///' + os.path.join(basedir, 'app.db')
